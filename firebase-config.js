74894ec034e8f082c0c094888f77e720857c2ec5
Sebastián <sebastian@psicoloquial.mx>
2026-03-18 15:32:58 -0600

    Integrar Firebase Auth y Firestore multiusuario para terapeutas

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+	apiKey: "AIzaSyD4fZ5PhzBz1VqkecOGSc8CeyDZMMeaWNI",
+	authDomain: "registro-de-pacientes-f729f.firebaseapp.com",
+	projectId: "registro-de-pacientes-f729f",
+	storageBucket: "registro-de-pacientes-f729f.firebasestorage.app",
+	messagingSenderId: "327033644024",
+	appId: "1:327033644024:web:38299c8aa2cf178207ac0d",
+	measurementId: "G-M1WWSGSH0V"
+};
